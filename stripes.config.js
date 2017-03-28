3d157e22f5d2450fa7e35c4bcc33affa40e5392c
Wolfram Schneider <wosch@indexdata.com>
2017-03-28 10:51:14 +0200

    folio ui tests

--- stripes.config.js
+++ stripes.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  okapi: { 
+	// 'url':'http://localhost:9130', 
+	'url':'http://folio-c10.aws.indexdata.com:9130', 
+	'tenant':'diku' 
+  },
   config: {
     reduxLog: true,
-    disableAuth: true
+    disableAuth: false
     // candidate logCategories: 'core,connect,redux,path,mpath,action,perm,xhr'
   },
   modules: {
     '@folio/trivial': {},
+    '@folio/items': {},
+    '@folio/scan': {},
     '@folio/users': {}
   }
 };
