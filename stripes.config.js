b569140f567482e41ed1930f0e5bb4c15858eb37
Mike Taylor <mike@indexdata.com>
2017-04-06 11:21:21 +0100

    Remove disableAuth setting, which will shortly be unsupported

--- stripes.config.js
+++ stripes.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: {
-    disableAuth: false
     // candidate logCategories: 'core,connect,redux,path,mpath,action,perm,xhr'
     // logPrefix
     // logTimestamp
