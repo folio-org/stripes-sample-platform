0e68d31eded10259c0d09305912b57c345a31b58
Mike Taylor <mike@indexdata.com>
2017-04-25 22:35:46 +0100

    More informative comments in config section

--- stripes.config.js
+++ stripes.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: {
-    // candidate logCategories: 'core,connect,redux,path,mpath,action,perm,xhr'
-    // logPrefix
-    // logTimestamp
-    // showPerms
-    // hasAllPerms
-    // softLogout (boolean)
+    // logCategories: 'core,connect,redux,path,mpath,action,perm,xhr'
+    // logPrefix: 'stripes'
+    // logTimestamp: false
+    // showPerms: false
+    // hasAllPerms: false
+    // softLogout: false
   },
   modules: {
     '@folio/trivial': {},
