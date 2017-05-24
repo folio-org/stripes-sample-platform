c215ea236007eedf64c1920299673f63ebda570d
Mike Taylor <mike@indexdata.com>
2017-05-24 15:41:32 +0100

    Add commented-out autoLogin and disableAuth lines

--- stripes.config.js
+++ stripes.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: {
+    // autoLogin: { username: 'diku_admin', password: 'admin' }
     // logCategories: 'core,connect,substitute,redux,path,mpath,action,perm,xhr'
     // logPrefix: 'stripes'
     // logTimestamp: false
     // showPerms: false
+    // disableAuth: false
     // hasAllPerms: false
     // softLogout: false
   },
