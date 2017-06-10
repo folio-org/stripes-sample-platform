4b674cb4ad41f7872726e6a30678969524e4e519
Mike Taylor <mike@indexdata.com>
2017-06-10 16:24:21 +0100

    Add "interface" to logCategories comment

--- stripes.config.js
+++ stripes.config.js
@@ -2,7 +2,7 @@ module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: {
     // autoLogin: { username: 'diku_admin', password: 'admin' }
-    // logCategories: 'core,connect,substitute,redux,path,mpath,action,perm,xhr'
+    // logCategories: 'core,connect,substitute,redux,path,mpath,action,perm,interface,xhr'
     // logPrefix: 'stripes'
     // logTimestamp: false
     // showPerms: false
