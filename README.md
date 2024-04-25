fe5ab94668acb09c084bcceaaa7fe65d91a5a9ca
Zak Burke <zburke@ebsco.com>
2024-04-25 11:56:03 -0400

    Ramsons package bumps and doc updates

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 
 This is an example of a Stripes "platform". It consists simply of an NPM [`package.json`](https://docs.npmjs.com/files/package.json) that specifies the version of `@folio/stripes` and of any Stripes modules you wish to make available to generate client bundles.
 
-Please see the [quick start guide](https://github.com/folio-org/stripes/blob/master/doc/quick-start.md) for more information.
+Please see the [Developing UI Modules with Stripes](https://folio-org.atlassian.net/wiki/x/DQBpCQ) for more information.
 
 The `stripes.config.js` is a configuration for a specific tenant. In general, a platform supports multiple tenants, each of which may include a different set of the available modules.
-Copy the `stripes.config.js` file to be your `stripes.config.js.local` configuration file.
+
+## Running the sample platform locally
+
+* Install NodeJS >= 18
+* Run `yarn`
+* Run `yarn stripes serve stripes.config.js`
+
+## Deploying the sample platform
+
+* Install NodeJS >=
+* Run `yarn`
+* Run `yarn stripes build stripes.config.js`
+* This will generate a bundle in `./output`; copy these assets to your HTTP server
--- stripes.config.js
+++ stripes.config.js
@@ -1,29 +1,72 @@
 module.exports = {
-  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
-  config: {
-    // autoLogin: { username: 'diku_admin', password: 'admin' }
-    // logCategories: 'core,redux,connect,connect-fetch,substitute,path,mpath,mquery,action,event,perm,interface,xhr'
-    // logPrefix: 'stripes'
-    // logTimestamp: false
-    // showPerms: false
-    // showHomeLink: false
-    // listInvisiblePerms: false
-    // disableAuth: false
-    // hasAllPerms: false
-    // maxUnpagedResourceCount: 2000
-    // suppressIntlErrors: true
-    // suppressIntlWarnings: true
+  okapi: {
+    // API gateway
+    'url': 'http://localhost:9130',
+
+    // tenant to include in X-Okapi-Tenant HTTP request headers
+    'tenant': 'diku'
   },
+  config: {
+    // stripes logger config
+    // logCategories: 'core,redux,connect,connect-fetch,substitute,path,mpath,mquery,action,event,perm,interface,xhr',
+    // logPrefix: 'stripes',
+    // logTimestamp: false,
+
+    // list permissions in the userprofile menu?
+    // showPerms: false,
+
+    // show the "home" link to / in the user-profile menu
+    // showHomeLink: false,
+    // listInvisiblePerms: false,
+
+    // act as though user has all perms, i.e. <IfPermission> always returns true
+    // default: FALSE
+    // hasAllPerms: false,
+
+    // autoLogin: { username: 'someusername', password: 'somepassword' },
+    // disableAuth: false,
+
+    // lookup table rows to retrieve en-masse
+    // maxUnpagedResourceCount: 2000,
+
+    // prevent the console from being cleared on logout
+    // preserveConsole: true,
+
+    // hide verbose react-intl complaints
+    // suppressIntlErrors: true,
+    // suppressIntlWarnings: true,
+
+    // use RTR instead of insecure legacy endpoints
+    // since Q, default: FALSE
+    // since R, default: TRUE, cannot be overridden
+    // useSecureTokens: true,
+  },
+  // UI modules to include
+  // the same modules must be direct-dependencies in package.json
   modules: {
     '@folio/users': {}
   },
   branding: {
+    // login page img and alt-text
     logo: {
       src: './tenant-assets/opentown-libraries-logo.png',
       alt: 'Opentown Libraries',
     },
+    // browser tab icon
     favicon: {
       src: './tenant-assets/opentown-libraries-favicon.png',
+    },
+    style: {
+      mainNav: {
+        // background color of the main navigation bar
+        // default: #000c
+        backgroundColor: "#036",
+      },
+      login: {
+        // background color of the login screen
+        // default: #fff
+        backgroundColor: "#fcb",
+      },
     },
   },
 };
